de31e7e903c7e5222127134c61f40d490ac50a52
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-04-10 10:55:10 -0600

    Creacion de modelos de las app main y user

--- main/models.py
+++ main/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+
+class Signo(models.Model):
+    nombre = models.CharField(max_length=255, unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = 'signos'
+
+class Sintoma(models.Model):
+    nombre = models.CharField(max_length=255, unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = 'sintomas'
+
+class PruebaLaboratorio(models.Model):
+    nombre = models.CharField(max_length=255, unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = 'pruebas_laboratorio'
+
+class PruebaPostmorten(models.Model):
+    nombre = models.CharField(max_length=255, unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = 'pruebas_postmorten'
+
+class Enfermedad(models.Model):
+    nombre = models.CharField(max_length=255, unique=True, null=False)
+    sintomas = models.ManyToManyField(Sintoma, db_table='enfermedades_sintomas', related_name='enfermedades')
+    signos = models.ManyToManyField(Signo, db_table='enfermedades_signos', related_name='enfermedades')
+    pruebasLaboratorio = models.ManyToManyField(PruebaLaboratorio, db_table='enfermedades_pruebas_laboratorio', related_name='enfermedades')
+    pruebasPostmorten = models.ManyToManyField(PruebaPostmorten, db_table='enfermedades_pruebas_postmorten', related_name='enfermedades')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = 'enfermedades'
--- user/models.py
+++ user/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from enum import Enum
+from django.contrib.auth.models import User
+from main.models import Enfermedad
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
+
+class Direccion(models.Model):
+    municipio = models.CharField(max_length=50, null=False) 
+    colonia = models.CharField(max_length=30, null=False)
+    codigoPostal = models.CharField(max_length=5, null=False)
+    calle = models.CharField(max_length=50, null=False)
+    numero = models.PositiveIntegerField(null=False)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'direcciones'
+
+
+class Consultorio(models.Model):
+    nombre = models.CharField(max_length=255, unique=True, null=False)
+    hora_abierto = models.TimeField(null=False) 
+    hora_cerrado = models.TimeField(null=False)
+    direccion = GenericRelation(Direccion)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'consultorios'
+
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=255, null=False)
+    apellidos = models.CharField(max_length=255, null=False)
+    telefono = models.CharField(max_length=10, null=False)
+    direccion = GenericRelation(Direccion)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'pacientes'
+
+class Cita(models.Model):
+    class EstadoCita(Enum):
+        PROGRAMADA = 'Programada'
+        CONFIRMADA = 'Confirmada'
+        EN_ESPERA = 'En espera'
+        EN_CURSO = 'En curso'
+        CANCELADA_PACIENTE = 'Cancelada por el paciente'
+        CANCELADA_MEDICO = 'Cancelada por el médico'
+        COMPLETADA = 'Completada'
+        NO_PRESENTADO = 'No presentado'
+
+    fecha = models.DateField(null=False)
+    hora = models.TimeField(null=False)
+    estado = models.CharField(max_length=20, choices=[(estado.name, estado.value) for estado in EstadoCita])
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='citas')
+    consultorio = models.ForeignKey(Consultorio, on_delete=models.CASCADE, related_name='citas')
+    secretaria = models.ForeignKey(User, on_delete=models.CASCADE, related_name='citasCreadas')
+    medico =  models.ForeignKey(User, on_delete=models.CASCADE, related_name='citasAsignadas')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'citas'
+
+class Consulta(models.Model):
+    motivo = models.TextField(null=False)
+    diagnostico = models.TextField(null=False)
+    tratamiento = models.TextField(null=False)
+    cita = models.OneToOneField(Cita, on_delete=models.CASCADE, related_name='consulta')
+    medico =  models.ForeignKey(User, on_delete=models.CASCADE, related_name='consultas')
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='consultas')
+    enfermedades = models.ManyToManyField(Enfermedad, db_table='consultas_enfermedades')
+
+    class Meta:
+        db_table = 'consultas'
+
+class HistorialConsultas(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='historial')
+    consulta = models.OneToOneField(Consulta, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'historial_consultas'
+
