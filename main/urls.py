e712ed40299c815cc708f366a31d9aac2b3d3aee
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-04-10 20:41:25 -0600

    Creacion de rutas

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,10 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path("", login_required(views.home_request), name="home"),
+    path("", login_required(views.home), name="home"),
+    path("sintomas", login_required(views.index_sintoma), name="sintomas-list"),
+    path("signos", login_required(views.index_signo), name="signos-list"),
+    path("pruebas", login_required(views.index_pruebas), name="pruebas-list"),
+    path("consultorios", login_required(views.index_consultorio), name="consultorios-list"),
+    path("pacientes", login_required(views.index_paciente), name="pacientes-list"),
 ]
