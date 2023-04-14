118d62b72f950899641d4c3fd9dc8f6fab699d4c
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-04-14 13:34:39 -0600

    Implementacion de crud de pruebas de laboratorio

--- main/forms.py
+++ main/forms.py
@@ -12,3 +12,9 @@ class SignoForm(forms.Form):
                 'required': _('Este campo nombre es requerido.'),
                 'max_length': _('El campo nombre debe tener como máximo 50 caracteres.'),
             })
+    
+class PruebaLaboratorioForm(forms.Form):
+    nombre = forms.CharField(max_length=50, required=True, error_messages={
+                'required': _('Este campo nombre es requerido.'),
+                'max_length': _('El campo nombre debe tener como máximo 50 caracteres.'),
+            })
--- unicorn/components/lists/pruebas_laboratorio_list.py
+++ unicorn/components/lists/pruebas_laboratorio_list.py
@@ -1,5 +1,68 @@
 from django_unicorn.components import UnicornView
-
+from main.models import PruebaLaboratorio
+from main.forms import PruebaLaboratorioForm
 
 class PruebasLaboratorioListView(UnicornView):
-    pass
+    pruebas = PruebaLaboratorio.objects.none()
+    prueba = None
+    isOpenModal = "hidden"
+
+    form_class = PruebaLaboratorioForm
+    nombre = ""
+
+    def mount(self):
+        self.load_table()
+
+    def hydrate(self):
+        self.load_table()
+
+    def load_table(self):
+        self.pruebas = PruebaLaboratorio.objects.all()
+
+    def open_modal(self, id=None):
+        print("Abierto")
+        self.form_class = PruebaLaboratorioForm
+        self.isOpenModal = ""
+
+        if id:
+            self.prueba = PruebaLaboratorio.objects.get(id=id)
+            self.nombre = self.prueba.nombre
+
+    def close_modal(self):
+        self.restablecer_valores()
+
+    def save_prueba(self):
+        if self.is_valid() and self.validar_nombre_unico():
+            if self.prueba:
+                self.prueba.nombre = self.nombre
+                self.prueba.save()
+                self.call("alerta_actualizacion_satisfactoria")
+            else:
+                prueba = PruebaLaboratorio(nombre=self.nombre)
+                prueba.save()
+                self.call("alerta_creacion_satisfactoria")
+
+            self.restablecer_valores()
+
+    def delete_prueba(self, id):
+        prueba = PruebaLaboratorio.objects.get(id=id)
+        prueba.delete()
+        self.call("alerta_eliminacion_satisfactoria")
+
+    def restablecer_valores(self):
+        self.nombre = ""
+        self.prueba = None
+        self.isOpenModal = "hidden"
+
+    def validar_nombre_unico(self):
+        if self.prueba:
+            if not self.prueba.nombre == self.nombre:
+                if PruebaLaboratorio.objects.filter(nombre=self.nombre).exists():
+                    self.call("alerta_nombre_existente")
+                    return False
+        else:
+            if PruebaLaboratorio.objects.filter(nombre=self.nombre).exists():
+                    self.call("alerta_nombre_existente")
+                    return False
+        
+        return True
