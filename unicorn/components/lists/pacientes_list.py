08edf03499d4947fbb538030cbce84d1adbc4217
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-04-19 21:35:57 -0600

    Implementacion de listado de pacientes

--- unicorn/components/lists/pacientes_list.py
+++ unicorn/components/lists/pacientes_list.py
@@ -1,5 +1,19 @@
 from django_unicorn.components import UnicornView
-
+from user.models import Paciente
 
 class PacientesListView(UnicornView):
-    pass
+    pacientes = Paciente.objects.none()
+
+    def mount(self):
+        self.load_table()
+
+    def hydrate(self):
+        self.load_table()
+
+    def load_table(self):
+        self.pacientes = Paciente.objects.all()
+
+    def delete_consultorio(self, id):
+        paciente = Paciente.objects.get(id=id)
+        paciente.delete()
+        self.call("alerta_eliminacion_satisfactoria")
