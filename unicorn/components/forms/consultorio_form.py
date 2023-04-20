d7d14f57c06862624c888a05ebc39682c1a1ade6
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-04-19 20:51:09 -0600

    Se permite la eliminacion y edicion

--- unicorn/components/forms/consultorio_form.py
+++ unicorn/components/forms/consultorio_form.py
@@ -15,22 +15,68 @@ class ConsultorioFormView(UnicornView):
     calle = ""
     numero = None
 
-    def save_consultorio(self):
-        if self.is_valid():
-            consultorio = Consultorio(
-                            nombre=self.nombre,
-                            hora_abierto = self.hora_abierto,
-                            hora_cerrado = self.hora_cerrado,
-                          )
-            consultorio.save()
+    def __init__(self, *args, **kwargs):
+        super().__init__(**kwargs)
+        self.consultorio = None if kwargs.get("consultorio") == 'consultorio' else kwargs.get("consultorio")
 
-            direccion = Direccion(
-                            municipio = self.municipio,
-                            colonia = self.colonia,
-                            codigoPostal = self.codigoPostal,
-                            calle = self.calle,
-                            numero = self.numero,
-                            content_object = consultorio,
-                        )
-            direccion.save()
+    def mount(self):
+        if self.consultorio:
+            self.nombre = self.consultorio.nombre
+            self.hora_abierto = self.consultorio.hora_abierto
+            self.hora_cerrado = self.consultorio.hora_cerrado
+            self.municipio = self.consultorio.direccion.all()[0].municipio
+            self.colonia = self.consultorio.direccion.all()[0].colonia
+            self.codigoPostal = self.consultorio.direccion.all()[0].codigoPostal
+            self.calle = self.consultorio.direccion.all()[0].calle
+            self.numero = self.consultorio.direccion.all()[0].numero
+
+    def save_consultorio(self):
+        if self.is_valid() and self.validar_nombre_unico():
+            if self.consultorio:
+                self.consultorio.nombre = self.nombre
+                self.consultorio.hora_abierto = self.hora_abierto
+                self.consultorio.hora_cerrrado = self.hora_cerrado
+                self.consultorio.save()
+
+                direccion = self.consultorio.direccion.all()[0]
+                direccion.municipio = self.municipio
+                direccion.colonia = self.colonia
+                direccion.codigoPostal = self.codigoPostal
+                direccion.calle = self.calle
+                direccion.numero = self.numero
+                direccion.save()
+
+                self.call("alerta_actualizacion_satisfactoria")
+            else:
+                consultorio = Consultorio(
+                                nombre=self.nombre,
+                                hora_abierto = self.hora_abierto,
+                                hora_cerrado = self.hora_cerrado,
+                            )
+                consultorio.save()
+
+                direccion = Direccion(
+                                municipio = self.municipio,
+                                colonia = self.colonia,
+                                codigoPostal = self.codigoPostal,
+                                calle = self.calle,
+                                numero = self.numero,
+                                content_object = consultorio,
+                            )
+                direccion.save()
+
+                self.call("alerta_creacion_satisfactoria")
+    
+    def validar_nombre_unico(self):
+        if self.consultorio:
+            if not self.consultorio.nombre == self.nombre:
+                if Consultorio.objects.filter(nombre=self.nombre).exists():
+                    self.call("alerta_nombre_existente")
+                    return False
+        else:
+            if Consultorio.objects.filter(nombre=self.nombre).exists():
+                    self.call("alerta_nombre_existente")
+                    return False
+        
+        return True
 
--- unicorn/components/lists/consultorios_list.py
+++ unicorn/components/lists/consultorios_list.py
@@ -7,5 +7,13 @@ class ConsultoriosListView(UnicornView):
     def mount(self):
         self.load_table()
 
+    def hydrate(self):
+        self.load_table()
+
     def load_table(self):
         self.consultorios = Consultorio.objects.all()
+
+    def delete_consultorio(self, id):
+        consultorio = Consultorio.objects.get(id=id)
+        consultorio.delete()
+        self.call("alerta_eliminacion_satisfactoria")
