50e18ed2b2fc675bcc13180bd6c40c01bfe01de7
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-04-19 19:11:59 -0600

    Permite la creacion de un consultorio

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class SintomaForm(forms.Form):
     nombre = forms.CharField(max_length=50, required=True, error_messages={
@@ -23,4 +24,36 @@ class PruebaPostmortenForm(forms.Form):
     nombre = forms.CharField(max_length=50, required=True, error_messages={
                 'required': _('Este campo nombre es requerido.'),
                 'max_length': _('El campo nombre debe tener como máximo 50 caracteres.'),
+            })
+    
+class ConsultorioForm(forms.Form):
+    nombre = forms.CharField(max_length=50, required=True, error_messages={
+                'required': _('Este campo nombre es requerido.'),
+                'max_length': _('El campo nombre debe tener como máximo 50 caracteres.'),
+            })
+    hora_abierto = forms.TimeField(required=True, error_messages={
+                'required': _('Este campo hora inicial es requerido.'),
+                'invalid': _('Ingrese un tiempo válido en el formato HH:MM:SS'),
+            })
+    hora_cerrado = forms.TimeField(required=True, error_messages={
+                'required': _('Este campo hora final es requerido.'),
+                'invalid': _('Ingrese un tiempo válido en el formato HH:MM:SS'),
+            })
+    municipio = forms.CharField(max_length=50, required=True, error_messages={
+                'required': _('Este campo municipio es requerido.'),
+                'max_length': _('El campo municipio debe tener como máximo 50 caracteres.'),
+            })
+    colonia = forms.CharField(max_length=30, required=True, error_messages={
+                'required': _('Este campo colonia es requerido.'),
+                'max_length': _('El campo colonia debe tener como máximo 50 caracteres.'),
+            })
+    codigoPostal = forms.IntegerField(max_value=50000, min_value=40000, required=True, error_messages={
+                'required': _('Este campo codigo postal es requerido.'),
+            })
+    calle = forms.CharField(max_length=50, required=True, error_messages={
+                'required': _('Este campo calle es requerido.'),
+                'max_length': _('El campo calle debe tener como máximo 50 caracteres.'),
+            })
+    numero = forms.IntegerField(max_value=50000, min_value=1, required=True, error_messages={
+                'required': _('Este campo numero es requerido.'),
             })
--- unicorn/components/forms/consultorio_form.py
+++ unicorn/components/forms/consultorio_form.py
@@ -1,5 +1,36 @@
 from django_unicorn.components import UnicornView
-
+from user.models import Consultorio, Direccion
+from main.forms import ConsultorioForm
 
 class ConsultorioFormView(UnicornView):
-    pass
+    consultorio = None
+
+    form_class = ConsultorioForm
+    nombre = ""
+    hora_abierto = None
+    hora_cerrado = None
+    municipio = ""
+    colonia = ""
+    codigoPostal = None
+    calle = ""
+    numero = None
+
+    def save_consultorio(self):
+        if self.is_valid():
+            consultorio = Consultorio(
+                            nombre=self.nombre,
+                            hora_abierto = self.hora_abierto,
+                            hora_cerrado = self.hora_cerrado,
+                          )
+            consultorio.save()
+
+            direccion = Direccion(
+                            municipio = self.municipio,
+                            colonia = self.colonia,
+                            codigoPostal = self.codigoPostal,
+                            calle = self.calle,
+                            numero = self.numero,
+                            content_object = consultorio,
+                        )
+            direccion.save()
+
