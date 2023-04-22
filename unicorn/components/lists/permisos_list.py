67e1df4ee43888a37b0bec09e0946ed803a8b1b3
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-04-21 18:24:49 -0600

    Implementacion de listado de permisos

--- unicorn/components/lists/permisos_list.py
+++ unicorn/components/lists/permisos_list.py
@@ -1,5 +1,14 @@
 from django_unicorn.components import UnicornView
-
+from django.contrib.auth.models import Permission
 
 class PermisosListView(UnicornView):
-    pass
+    permisos = Permission.objects.none()
+
+    def mount(self):
+        self.load_table()
+
+    def hydrate(self):
+        self.load_table()
+
+    def load_table(self):
+        self.permisos = Permission.objects.all()
