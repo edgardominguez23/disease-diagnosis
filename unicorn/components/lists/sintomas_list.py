a3fd2d2cbf9f555b5e2d4171b10bb4cb2318648f
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-05-02 21:19:45 -0600

    Cambios en la interfaz

--- unicorn/components/lists/sintomas_list.py
+++ unicorn/components/lists/sintomas_list.py
@@ -1,14 +1,27 @@
 from django_unicorn.components import UnicornView
+from django.core.paginator import Paginator
 from main.models import Sintoma
 from main.forms import SintomaForm
 
 class SintomasListView(UnicornView):
-    sintomas = Sintoma.objects.none()
     sintoma = None
     isOpenModal = "hidden"
 
+    page_index = 1
+    paginator = None
+    page = None
+    page_range = None
+
     form_class = SintomaForm
     nombre = ""
+
+    class Meta:
+        exclude = ()
+        javascript_exclude = (
+            "paginator",
+            "page",
+            "page_range",
+        )
 
     def mount(self):
         self.load_table()
@@ -17,7 +30,9 @@ class SintomasListView(UnicornView):
         self.load_table()
 
     def load_table(self):
-        self.sintomas = Sintoma.objects.all()
+        qs = Sintoma.objects.order_by('id')
+        self.paginator = Paginator(qs, 10)
+        self.page = self.paginator.page(self.page_index)
 
     def open_modal(self, id=None):
         self.form_class = SintomaForm
@@ -65,3 +80,17 @@ class SintomasListView(UnicornView):
                     return False
         
         return True
+    
+    def cambiar_pagina(self, number):
+        self.page_index = number
+        self.load_table()
+
+    def siguiente_pagina(self):
+        if self.page.has_next():
+            self.page_index += 1
+            self.load_table()
+
+    def regresar_pagina(self):
+        if self.page.has_previous():
+            self.page_index -= 1
+            self.load_table()
