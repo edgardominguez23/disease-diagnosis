27781d34d0f6e32045cb7bcdeaa397bbc1bc4524
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-05-06 12:50:49 -0600

    Se agrega el listado de citas

--- unicorn/components/lists/citas_list.py
+++ unicorn/components/lists/citas_list.py
@@ -1,5 +1,19 @@
 from django_unicorn.components import UnicornView
-
+from user.models import Cita
 
 class CitasListView(UnicornView):
-    pass
+    citas = Cita.objects.none()
+
+    def mount(self):
+        self.load_table()
+
+    def hydrate(self):
+        self.load_table()
+
+    def load_table(self):
+        self.citas = Cita.objects.all()
+
+    def delete_paciente(self, id):
+        cita = Cita.objects.get(id=id)
+        cita.delete()
+        self.call("alerta_eliminacion_satisfactoria")
