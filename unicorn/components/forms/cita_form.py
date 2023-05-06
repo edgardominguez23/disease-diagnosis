9e5d6f4536e05100d20a5d0b6084a8dfab94f589
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-05-06 16:15:44 -0600

    Se permite la edicion de una cita

--- unicorn/components/forms/cita_form.py
+++ unicorn/components/forms/cita_form.py
@@ -2,6 +2,7 @@ from django_unicorn.components import UnicornView
 from user.models import Cita, Paciente, Consultorio
 from django.contrib.auth.models import User
 from main.forms import CitaForm
+from datetime import datetime
 
 class CitaFormView(UnicornView):
     cita = None
@@ -29,6 +30,14 @@ class CitaFormView(UnicornView):
     def mount(self):
         self.load_data()
 
+        if self.cita:
+            self.consultorio = self.cita.consultorio.id
+            self.paciente = self.cita.paciente.id
+            self.medico = self.cita.medico.id
+            self.fecha = self.cita.fecha
+            self.hora = self.cita.hora
+            self.estado = self.cita.estado
+
     def load_data(self):
         self.estados = [member.value for name, member in Cita.EstadoCita.__members__.items()]
         self.pacientes = Paciente.objects.all()
@@ -38,7 +47,16 @@ class CitaFormView(UnicornView):
     def save_cita(self):
         if self.is_valid():
             if self.cita:
-                print(self.cita)
+                self.cita.fecha = self.fecha
+                self.cita.hora = self.hora
+                self.cita.paciente = Paciente.objects.get(id=self.paciente)
+                self.cita.medico = User.objects.get(id=self.medico)
+                self.cita.secretaria = User.objects.get(id=self.secretario.id)
+                self.cita.consultorio = Consultorio.objects.get(id=self.consultorio)
+                self.cita.estado = self.estado
+                self.cita.save()
+
+                self.call("alerta_actualizacion_satisfactoria")
             else:
                 cita = Cita(
                     fecha = self.fecha,
