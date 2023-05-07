937c518b95ce842d06f17ebc2773c47aec791a61
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-05-07 15:19:39 -0600

    Se guarda la consulta

--- unicorn/components/forms/consulta_form.py
+++ unicorn/components/forms/consulta_form.py
@@ -1,20 +1,26 @@
 from django_unicorn.components import UnicornView
-from ai.inferenceEngine.learning import show_symptoms, show_signs
+from ai.inferenceEngine.learning import show_symptoms, show_signs, show_lab_tests, show_postmortem_tests
 from ai.inferenceEngine.engine import diagnose
 from multiprocessing import Process, Queue
 from queue import Empty
+from django.shortcuts import redirect
 
 class ConsultaFormView(UnicornView):
     cita = None
     symptoms = show_symptoms()
     signs = show_signs()
+    lab_tests = show_lab_tests()
+    postmortem_tests = show_postmortem_tests()
 
     diagnistico = ""
     tratamiento = ""
+    estaVivo = True
+    motivo = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
         self.cita = None if kwargs.get("cita") == 'cita' else kwargs.get("cita")
+        self.estaVivo = self.cita.paciente.estaVivo
 
     def get_consulta(self, padecimientos):
         queue = Queue()
@@ -31,3 +37,17 @@ class ConsultaFormView(UnicornView):
             print(self.diagnistico)
         except Empty:
             self.diagnistico = "No se encontraron resultados en el tiempo especificado."
+    
+    def guardar_consulta(self):
+        if self.tratamiento and self.diagnistico:
+            consulta = self.cita.consulta
+
+            consulta.motivo = self.motivo
+            consulta.diagnostico = self.diagnistico
+            consulta.tratamiento = self.tratamiento
+            consulta.save()
+
+            self.cita.estado = "Completada"
+            self.cita.save()
+
+            return redirect(f"/admin/citas")
