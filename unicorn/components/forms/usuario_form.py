766fdd8a04aef68cc2c01be58e56a65a66434949
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-05-07 20:26:00 -0600

    Solucion a error al crear y editar usuario

--- unicorn/components/forms/usuario_form.py
+++ unicorn/components/forms/usuario_form.py
@@ -1,28 +1,33 @@
 from django_unicorn.components import UnicornView
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from main.forms import UsuarioForm
 
 class UsuarioFormView(UnicornView):
     usuario = None
+    roles = ""
 
     form_class = UsuarioForm
     username = ""
     first_name = ""
     last_name = ""
     email = ""
-    password1 = ""
-    password2 = ""
+    password = ""
+    rol = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
-        self.usuario = None if kwargs.get("usuario") == 'usuario' else kwargs.get("usuario")
+        usuario = None if kwargs.get("usuario") == 'usuario' else kwargs.get("usuario")
+        self.usuario =  None if usuario == None else User.objects.get(id=usuario.id)
 
     def mount(self):
+        self.roles = Group.objects.all()
+
         if self.usuario:
             self.username = self.usuario.username
             self.first_name = self.usuario.first_name
             self.last_name = self.usuario.last_name
             self.email = self.usuario.email
+            self.rol = self.usuario.groups.first().id
 
     def save_usuario(self):
         if self.is_valid() and self.validar_username_unico():
@@ -31,7 +36,14 @@ class UsuarioFormView(UnicornView):
                 self.usuario.first_name = self.first_name
                 self.usuario.last_name = self.last_name
                 self.usuario.email = self.email
+
+                if self.password:
+                    self.usuario.password = self.password
+
                 self.usuario.save()
+
+                grupo = Group.objects.get(id=self.rol)
+                self.usuario.groups.add(grupo)
 
                 self.call("alerta_actualizacion_satisfactoria")
             else:
@@ -40,9 +52,12 @@ class UsuarioFormView(UnicornView):
                                 first_name = self.first_name,
                                 last_name = self.last_name,
                                 email = self.email,
-                                password = self.password1,
+                                password = self.password if self.password else "1234567890"
                             )
                 usuario.save()
+
+                grupo = Group.objects.get(id=self.rol)
+                usuario.groups.add(grupo)
 
                 self.call("alerta_creacion_satisfactoria")
 
