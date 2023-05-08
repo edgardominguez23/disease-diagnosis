3dfcdcef9cc673ffd1db3987841439b6ea97990d
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-05-07 20:37:36 -0600

    Solucion de error al cerrar sesion

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 from . import views
 
 urlpatterns = [
+    path('', views.main_request, name="raiz"),
     path("register", views.register_request, name="register"),
     path('login', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
-    path('', views.main_request),
+    path('logout', auth_views.LogoutView.as_view(next_page=reverse_lazy('raiz'), template_name='auth/logout.html'), name='logout'),
 ]
